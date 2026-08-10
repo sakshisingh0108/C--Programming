a5512ba9966d1c481bc1988dada23c98e3d50730
Sakshi Singh <187sakshisingh@gmail.com>
2026-08-10 10:17:19 +0530

    Create reverse_array.c

---
+++ Arrays/reverse_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements in reverse order:\n");
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
